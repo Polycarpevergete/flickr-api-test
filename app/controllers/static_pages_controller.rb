19e68b33e25c4dd63a578052377525f187baf25d
polycarpevergete <xavier.sassi@gmail.com>
2019-03-21 12:53:30 +0700

    still 302 pb. Need to investigate further

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
 
+require 'flickraw'
+
+flickr = FlickRaw::Flickr.new "fe604483860b654d27e7000dc8a1w6661", "a98616f282415f6f"
+
 def index
   flickr_user_id = search_params[:flickr_user_id] if search_params
   flickr_user_id = "25478444@N06" if flickr_user_id.blank?
@@ -7,7 +11,6 @@ def index
       @pics = flickr.people.getPhotos(user_id: flickr_user_id, extras: "url_m", per_page: 25)
     rescue FlickRaw::FailedResponse
       flash[:notice] = "Invalid user"
-      redirect_to index_path
     end
 end
     
